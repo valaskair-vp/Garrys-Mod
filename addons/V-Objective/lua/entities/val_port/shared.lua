7e58c0b47e5e400727e74523a7046b385af5d97b
valaskair-vp <veritas-valaskair@gmail.com>
2015-07-31 22:43:08 -0700

    Create shared.lua

---
+++ addons/V-Objective/lua/entities/val_port/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "Port"
+ENT.Author			= "Valaskair"
+ENT.Contact			= "http://www.steamcommunity.com/id/Valaskair"
+ENT.Purpose			= ""
+ENT.Instructions	= ""
+ENT.Spawnable		= true
+ENT.AdminSpawnable 	= true
+ENT.Category		= "Valaskair"
