ab8f0535843367e473827e961b1fae3a1fdabcda
valaskair-vp <veritas-valaskair@gmail.com>
2015-07-31 22:39:43 -0700

    Create shared.lua

---
+++ addons/V-CAC/lua/entities/val_cac/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "Ammo Crate"
+ENT.Author			= "Valaskair"
+ENT.Contact			= "http://www.steamcommunity.com/id/Valaskair"
+ENT.Purpose			= "Configurable Ammo Crate"
+ENT.Instructions	= ""
+ENT.Spawnable		= true
+ENT.AdminSpawnable 	= true
+ENT.Category		= "Valaskair"
