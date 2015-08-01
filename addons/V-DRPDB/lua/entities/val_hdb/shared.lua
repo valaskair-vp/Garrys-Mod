a770881635799519b34e9c01754f90fbac9eefc4
valaskair-vp <veritas-valaskair@gmail.com>
2015-07-31 22:38:03 -0700

    Create shared.lua

---
+++ addons/V-DRPDB/lua/entities/val_hdb/shared.lua
@@ -0,0 +1,11 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName		= "Donation Box"
+ENT.Author			= "Valaskair"
+ENT.Contact			= "http://www.steamcommunity.com/id/Valaskair"
+ENT.Purpose			= "Donation Box for DarkRP"
+ENT.Instructions	= "Money collided with it will be only usable by the owner."
+ENT.Spawnable		= true
+ENT.AdminSpawnable 	= true
+ENT.Category		= "Valaskair"
