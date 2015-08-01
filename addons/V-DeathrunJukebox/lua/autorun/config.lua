23fc7fefeafdb2305fc26f9afc0a450042c15f55
valaskair-vp <veritas-valaskair@gmail.com>
2015-07-31 22:44:26 -0700

    Create config.lua

---
+++ addons/V-DeathrunJukebox/lua/autorun/config.lua
@@ -0,0 +1,16 @@
+AudioPlaylist = {}
+AudioPlaylist[1] = "Bullseye|3r7-g5fOF2c|250"
+AudioPlaylist[2] = "Careless Whisper|Gluxwc8h334|93"
+AudioPlaylist[3] = "Starry Eyed|Kzlsqr7hIEA|120"
+AudioPlaylist[4] = "Can't Get You Out Of My Head|iTBs3vaVnvM|112"
+AudioPlaylist[5] = "American Boy|KsAyuOFs9eA|96"
+AudioPlaylist[6] = "Sweather Weather|fk5t_jw0vKQ|163"
+AudioPlaylistCount = (table.Count(AudioPlaylist))
+
+// AudioPlaylist[Unused Number] = "Title|ID|Duration"
+// The duration must be in seconds  | is used so titles can include spaces
+
+--[[ The ID is 11 characters usually after the v=
+	https://www.youtube.com/watch?v=3r7-g5fOF2c
+	ID = 3r7-g5fOF2c
+]]--
